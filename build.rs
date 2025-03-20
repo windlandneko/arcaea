fe1b6ecc6a4bc6f9cb375bfd3b58eddcc3ecb174
Charlie <ch.ar.lie@outlook.com>
2025-03-20 22:38:56 +0800

    refactor: simplify commit info retrieval in build script

--- build.rs
+++ build.rs
@@ -1,41 +1,24 @@
 use std::process::Command;
 
-struct CommitInfo {
-    hash: String,
-    short_hash: String,
-    date: String,
-}
-
-fn commit_info_from_git() -> Option<CommitInfo> {
-    let output = Command::new("git")
+fn commit_info_from_git() -> Option<String> {
+    Command::new("git")
         .args([
             "log",
             "-1",
             "--date=short",
-            "--format=%H %h %cd",
-            "--abbrev=9",
+            "--format= (%h %cd)",
+            "--abbrev=8",
         ])
         .output()
         .ok()
         .filter(|output| output.status.success())
-        .map(|output| String::from_utf8_lossy(&output.stdout).to_string())?;
-
-    let mut parts = output.split_whitespace().map(|s| s.to_string());
-
-    Some(CommitInfo {
-        hash: parts.next()?,
-        short_hash: parts.next()?,
-        date: parts.next()?,
-    })
+        .map(|output| String::from_utf8_lossy(&output.stdout).to_string())
 }
 
 fn main() {
-    let version = commit_info_from_git()
-        .map(|info| format!(" ({} {} {})", info.hash, info.short_hash, info.date))
-        .unwrap_or_default();
-
     println!(
-        "cargo:rustc-env=VERSION_INFO={}{version}",
-        env!("CARGO_PKG_VERSION")
+        "cargo:rustc-env=VERSION_INFO={}{}",
+        env!("CARGO_PKG_VERSION"),
+        commit_info_from_git().unwrap_or_default()
     );
 }
